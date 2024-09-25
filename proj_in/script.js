963c76d92a69cb3a5dcfda2486e7e0197b401be5
RISHIKA-A-S <rishika.as24@gmail.com>
2024-09-26 03:08:43 +0530

    Add files via upload

---
+++ proj_in/script.js
@@ -0,0 +1,19 @@
+function changeworld() {
+    document.getElementById("para").textContent = "THANK YOU FOR YOUR FEEDBACK";
+}
+
+function displayGreeting() {
+    const now = new Date();
+    const hours = now.getHours();
+    let greeting;
+
+    if (hours < 12) {
+        greeting = "Good morning!";
+    } else if (hours < 18) {
+        greeting = "Good afternoon!";
+    } else {
+        greeting = "Good evening!";
+    }
+
+    alert(greeting);
+}
